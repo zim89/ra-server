8a213f316e1499443beba95947b1afc4a02c2355
zim89 <121241841+zim89@users.noreply.github.com>
2023-08-04 15:57:35 +0300

    add node CRUD

--- models/notes.js
+++ models/notes.js
@@ -1,16 +1,71 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const path = require("path");
+const { nanoid } = require("nanoid");
+const moment = require("moment");
 
-const listNotes = async () => {};
+const notesPath = path.join(__dirname, "notes.json");
 
-const getNoteById = async (noteId) => {};
+const listNotes = async () => {
+  const result = await fs.readFile(notesPath);
+  return JSON.parse(result);
+};
 
-const removeNote = async (noteId) => {};
+const getNoteById = async (noteId) => {
+  const notes = await listNotes();
+  const result = notes.find(({ id }) => id === noteId);
+  return result || null;
+};
 
-const addNote = async (body) => {};
+const removeNote = async (id) => {
+  const notes = await listNotes();
+  const index = notes.findIndex((note) => note.id === id);
+  if (index === -1) return null;
+  const deleteNote = notes[index];
+  notes.splice(index, 1);
+  await fs.writeFile(notesPath, JSON.stringify(notes, null, 2));
+  return deleteNote;
+};
 
-const updateNote = async (noteId, body) => {};
+const addNote = async (body) => {
+  const notes = await listNotes();
+  const { name, category, content, date } = body;
+  const dates = [];
 
-export default {
+  if (moment(date).isValid()) {
+    dates.push(moment(date).format("DD.MM.YYYY"));
+  }
+
+  const newNote = {
+    id: nanoid(),
+    created_at: moment().format("DD.MM.YYYY hh:mm:ss"),
+    name,
+    category,
+    content,
+    dates,
+    isArchived: false,
+  };
+  notes.push(newNote);
+  await fs.writeFile(notesPath, JSON.stringify(notes, null, 2));
+  return newNote;
+};
+
+const updateNote = async (id, body) => {
+  const { name, category, content, date, isArchived } = body;
+  const notes = await listNotes();
+  const index = notes.findIndex((note) => note.id === id);
+
+  if (index === -1) return null;
+
+  if (moment(date).isValid()) {
+    notes[index].dates.push(moment(date).format("DD.MM.YYYY"));
+  }
+
+  notes[index] = { ...notes[index], name, category, content, isArchived };
+  await fs.writeFile(notesPath, JSON.stringify(notes, null, 2));
+  return notes[index];
+};
+
+module.exports = {
   listNotes,
   getNoteById,
   removeNote,
--- routes/api/notes.js
+++ routes/api/notes.js
@@ -1,25 +1,78 @@
 const express = require("express");
+const Joi = require("joi");
+const notes = require("../../models/notes");
+const { HttpError } = require("../../helpers");
 
 const router = express.Router();
 
+const addNoteSchema = Joi.object({
+  name: Joi.string().trim().required(),
+  category: Joi.string().trim().required(),
+  content: Joi.string().trim().required(),
+  date: Joi.any(),
+});
+
+const updateNoteSchema = Joi.object({
+  name: Joi.string().trim().required(),
+  category: Joi.string().trim().required(),
+  content: Joi.string().trim().required(),
+  date: Joi.any(),
+  isArchived: Joi.boolean().required(),
+});
+
 router.get("/", async (req, res, next) => {
-  res.json({ message: "Get all notes" });
+  try {
+    const result = await notes.listNotes();
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.get("/:noteId", async (req, res, next) => {
-  res.json({ message: "Get note by id" });
+  try {
+    const { noteId } = req.params;
+    const result = await notes.getNoteById(noteId);
+    if (!result) throw HttpError(404, "Note not found");
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.post("/", async (req, res, next) => {
-  res.json({ message: "Add note" });
+  try {
+    const { error } = addNoteSchema.validate(req.body);
+    if (error) throw HttpError(400, error.message);
+    const result = await notes.addNote(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.delete("/:noteId", async (req, res, next) => {
-  res.json({ message: "Delete note" });
+  try {
+    const { noteId } = req.params;
+    const result = await notes.removeNote(noteId);
+    if (!result) throw HttpError(404, "Note not found");
+    res.json({ message: "Delete success" });
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.put("/:noteId", async (req, res, next) => {
-  res.json({ message: "Update note" });
+router.patch("/:noteId", async (req, res, next) => {
+  try {
+    const { error } = updateNoteSchema.validate(req.body);
+    if (error) throw HttpError(400, error.message);
+    const { noteId } = req.params;
+    const result = await notes.updateNote(noteId, req.body);
+    if (!result) throw HttpError(404, "Note not found");
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
