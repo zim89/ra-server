613996021fdb6242c79bc8356f3b2d520195f45d
zim89 <121241841+zim89@users.noreply.github.com>
2023-08-10 15:50:29 +0300

    add docker

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: parseInt(process.env.DB_PORT || "5432"),
+});
+
+const connectToDB = async () => {
+  try {
+    await pool.connect();
+  } catch (err) {
+    console.log(err);
+  }
+};
+connectToDB();
 
 const notesRouter = require("./routes/api/notes");
 
